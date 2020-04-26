5762c1975a829127716fe8bb8eea37168a131d72
AleehaZafar <21100091@lums.edu.pk>
2020-04-26 12:04:49 +0500

    src code

---
+++ src/components/login/login.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import './login.css'
+import logo from './img/pngformat.png';
+import axios from 'axios';
+
+class Counter extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            Username : '',
+            Password : '',
+        }
+        this.handleClick = this.handleClick.bind(this);
+        this.inputChange = this.inputChange.bind(this);
+    }   
+    inputChange(e) {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+    handleClick = (event) => {
+        event.preventDefault()
+        // if (this.state.Username === 'hammad_arif021@hotmail.com' && this.state.Password === '12345678'){
+        //      this.props.history.push('/loading')
+        // } 
+        var apiBaseUrl =  "http://localhost:4000/api/";
+        var self = this;
+        var payload = {
+            'username': this.state.Username,
+            'password': this.state.Password
+        }
+        axios.post(apiBaseUrl+'login', payload)
+        .then(function(response){
+            if (response.data.code === 200)
+            {
+                self.props.history.push('/loading');
+            }
+        })
+        return 
+    }
+    render() { 
+        return ( 
+        <div className="container-login">
+        <div className="wrap-login">
+            <img src={logo} alt="logo"/>
+            <div id="CUTTING_EDGE__SOLUTION__to_all">
+							<h2>CUTTING-EDGE<br/>SOLUTION</h2><h1>to all your </h1><h2>HR</h2><h1>needs</h1>
+            </div>
+            <form className="login-form validate-form">
+                <span className="login-form-title">
+                    Login
+                </span>
+
+                <div className="wrap-input100 validate-input">
+                    <input className="input100" type="text" name="Username" placeholder="Username" required onChange = {this.inputChange} ></input>
+                </div>
+
+                <div className="wrap-input100 validate-input">
+                    <input className="input100" type="password" name="Password" placeholder="Password" required onChange = {this.inputChange}></input>
+                </div>
+                
+                <div className="container-login-form-btn">
+                    <button className="login-form-btn" onClick={this.handleClick} >
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+        </div>
+        );
+    }
+}
+ 
+export default Counter;

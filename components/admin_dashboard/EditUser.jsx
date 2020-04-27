e3d1c7729054d594c859dab155fec857ea33d308
OsamaYounus20 <21100013@lums.edu.pk>
2020-04-27 09:10:13 +0500

    Updates layout

--- components/admin_dashboard/EditUser.jsx
+++ components/admin_dashboard/EditUser.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import './EditUser.css'
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-import Navbar from './Navbar/Navbar'
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Avatar from '@material-ui/core/Avatar';
-import image1 from './img/imagetest.jpg';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { green} from '@material-ui/core/colors';
+import React from "react";
+import "./EditUser.css";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Navbar from "./Navbar/Navbar";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Avatar from "@material-ui/core/Avatar";
+import image1 from "./img/imagetest.jpg";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { green } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& .MuiTextField-root': {
+    "& .MuiTextField-root": {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: "25ch",
     },
   },
   margin: {
     margin: theme.spacing(1),
   },
   image: {
-    display: 'flex',
-    '& > *': {
-    margin: theme.spacing(2),
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(2),
     },
   },
   large: {
@@ -33,52 +33,131 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 const theme = createMuiTheme({
-    palette: {
-      primary: green,
-    },
-  });
+  palette: {
+    primary: green,
+  },
+});
 export default function FormPropsTextFields() {
   const classes = useStyles();
 
   return (
     <div>
-    <Navbar />
-    <div className="ContainerPlacing">
-    <Container fixed>
-        <h1>Edit Personal Info</h1>
-        <Typography component="div" style={{ backgroundColor: '#fff', height: 'auto' }}>
-        <form className={classes.root}>
-        <div>
-            <div className={classes.image}>
-            <Avatar alt="Remy Sharp" src={image1} className={classes.large}  />
-            </div>
-            <TextField id="outlined-basic" label="Name" variant="outlined" defaultValue="Hammad Arif"/>
-            <TextField id="outlined-basic" label="Email" variant="outlined" defaultValue="kiun bataon"/>
-            <TextField id="outlined-basic" label="Blood Group" variant="outlined" defaultValue="kiun bataon"/>
-            <TextField id="outlined-basic" label="Phone Number" variant="outlined" defaultValue="kiun bataon"/>
-            <TextField id="outlined-basic" label="Nationality" variant="outlined" defaultValue="kiun bataon"/>
-            <TextField id="outlined-basic" label="CNIC" variant="outlined" defaultValue="kiun bataon"/>
-            <TextField id="outlined-basic" label="D.O.B" variant="outlined" defaultValue="kiun bataon"/>
-            <TextField id="outlined-basic" label="Marital Status" variant="outlined" defaultValue="kiun bataon"/>
-            <TextField id="outlined-basic" label="Designation" variant="outlined" defaultValue="kiun bataon"/>
-            <TextField id="outlined-basic" label="Department" variant="outlined" defaultValue="kiun bataon"/>
-            <TextField id="outlined-basic" label="Location" variant="outlined" defaultValue="kiun bataon"/> 
-            <TextField id="outlined-multiline-static" label="Address" rows={4} multiline variant="outlined" defaultValue="kiun bataon"/>
+      <Navbar />
+      <div className="ContainerPlacing">
+        <Container fixed>
+          <h1>Edit Personal Info</h1>
+          <Typography
+            component="div"
+            style={{ backgroundColor: "#fff", height: "auto" }}
+          >
+            <form className={classes.root}>
+              <div>
+                <div className={classes.image}>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={image1}
+                    className={classes.large}
+                  />
+                </div>
+                <TextField
+                  id="outlined-basic"
+                  label="Name"
+                  variant="outlined"
+                  defaultValue="John Doe"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Email"
+                  variant="outlined"
+                  defaultValue="johndoe@johndoe.com"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Phone Number"
+                  variant="outlined"
+                  defaultValue="090078601"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="CNIC"
+                  variant="outlined"
+                  defaultValue="12345-12345678-6"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="D.O.B"
+                  variant="outlined"
+                  defaultValue="19 June 1980"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Marital Status"
+                  variant="outlined"
+                  defaultValue="Married"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Blood Group"
+                  variant="outlined"
+                  defaultValue="B+"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Designation"
+                  variant="outlined"
+                  defaultValue="Manager"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Department"
+                  variant="outlined"
+                  defaultValue="HR"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Nationality"
+                  variant="outlined"
+                  defaultValue="Pakistan"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Location"
+                  variant="outlined"
+                  defaultValue="Karachi Head Office"
+                />
+                <TextField
+                  id="outlined-multiline-static"
+                  label="Address"
+                  rows={4}
+                  multiline
+                  variant="outlined"
+                  defaultValue="192-B, Bakers Street, Downtown, Karachi"
+                />{" "}
+              </div>
+            </form>
+          </Typography>
+        </Container>
+        <div className="centerplacing">
+          <ThemeProvider theme={theme}>
+            <Button
+              variant="contained"
+              size="small"
+              color="primary"
+              className={classes.margin}
+            >
+              Save
+            </Button>
+            <Button
+              variant="contained"
+              size="small"
+              color="primary"
+              className={classes.margin}
+            >
+              Cancel
+            </Button>
+          </ThemeProvider>
         </div>
-        </form>
-        </Typography>
-    </Container>
-    <div className="centerplacing">
-    <ThemeProvider theme={theme}>
-        <Button variant="contained" size ="small" color="primary" className={classes.margin}>
-          Save
-        </Button>
-        <Button variant="contained" size ="small" color="primary" className={classes.margin}>
-          Cancel
-        </Button>
-      </ThemeProvider>
-    </div>
-    </div>
+      </div>
     </div>
   );
 }
